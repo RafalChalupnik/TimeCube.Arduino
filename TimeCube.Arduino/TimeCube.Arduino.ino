fdaa50bf41d76e079f2a470fbed797566d613e91
Rafał Chałupnik <rafal.chalupnik@gmail.com>
2021-02-18 20:54:05 +0100

    Basic calibration flag get/set

--- TimeCube.Arduino/TimeCube.Arduino.ino
+++ TimeCube.Arduino/TimeCube.Arduino.ino
@@ -1,5 +1,9 @@
 #include <EEPROM.h>
 
+const int CALIBRATED_ADDRESS = 0;
+
+const int CALIBRATED_FLAG = 178;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -10,17 +14,36 @@ void loop() {
 
   if (Serial.available() > 0) {
     String receivedString = Serial.readString();
-    char buffer[receivedString.length()];
-    receivedString.toCharArray(buffer, receivedString.length());
-    int value = atoi(buffer);
-    Serial.println("Writing to EEPROM:");
-    Serial.println(value);
-    EEPROM.write(0, value);
+
+    if (receivedString.indexOf("calibrated") != -1) {
+      Serial.println("Setting calibrated to true...");
+      setCalibrated(true);
+    } else if (receivedString.indexOf("notCalibrated") != 1) {
+      Serial.println("Setting calibrated to false...");
+      setCalibrated(false);
+    } else {
+      Serial.println("Command not recognized.");
+    }
   } else {
-    int value = EEPROM.read(0);
-    Serial.println("Reading from EEPROM:");
-    Serial.println(value);
+    bool calibrated = getCalibrated();
+    Serial.println("Reading Calibrated from EEPROM:");
+    Serial.println(calibrated);
   }
 
   delay(1000);
+}
+
+// Memory handling
+
+bool getCalibrated() {
+  int flag = EEPROM.read(CALIBRATED_ADDRESS);
+  return flag == CALIBRATED_FLAG;
+}
+
+bool setCalibrated(bool calibrated) {
+  if (calibrated) {
+    EEPROM.write(CALIBRATED_ADDRESS, CALIBRATED_FLAG);
+  } else {
+    EEPROM.write(CALIBRATED_ADDRESS, 0);
+  }
 }
