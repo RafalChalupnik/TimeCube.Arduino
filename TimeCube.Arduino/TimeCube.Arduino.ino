1b10e298c0ca1a55943703dd84ace38aa7848663
Rafał Chałupnik <rafal.chalupnik@gmail.com>
2021-02-18 20:40:34 +0100

    EEPROM read/write

--- TimeCube.Arduino/TimeCube.Arduino.ino
+++ TimeCube.Arduino/TimeCube.Arduino.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,9 +7,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available() > 0) {
-    String receivedString = Serial.readString();
 
-    Serial.println("I've received: '" + receivedString + "'");
+  if (Serial.available() > 0) {
+    String receivedString = Serial.readString();
+    char buffer[receivedString.length()];
+    receivedString.toCharArray(buffer, receivedString.length());
+    int value = atoi(buffer);
+    Serial.println("Writing to EEPROM:");
+    Serial.println(value);
+    EEPROM.write(0, value);
+  } else {
+    int value = EEPROM.read(0);
+    Serial.println("Reading from EEPROM:");
+    Serial.println(value);
   }
+
+  delay(1000);
 }
