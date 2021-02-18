5fb63dc9c51b945fb6bcb112dd104fa971ab425c
Rafał Chałupnik <rafal.chalupnik@gmail.com>
2021-02-18 20:25:12 +0100

    Basic serial communication

--- TimeCube.Arduino/TimeCube.Arduino.ino
+++ TimeCube.Arduino/TimeCube.Arduino.ino
@@ -1,9 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    String receivedString = Serial.readString();
 
+    Serial.println("I've received: '" + receivedString + "'");
+  }
 }
