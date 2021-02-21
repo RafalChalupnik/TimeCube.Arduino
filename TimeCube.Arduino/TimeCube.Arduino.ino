b2f326796864adf3cf66383eca92014f495f442d
Rafał Chałupnik <rafal.chalupnik@gmail.com>
2021-02-21 22:42:08 +0100

    Further improvements

--- TimeCube.Arduino/TimeCube.Arduino.ino
+++ TimeCube.Arduino/TimeCube.Arduino.ino
@@ -1,104 +1,236 @@
 #include <EEPROM.h>
 
-const int MAX_SIDE_COUNT = 25;
 const int INT_MAX_VALUE = 10000;
+const int INTERVAL = 1000;
+const int MAX_SIDE_COUNT = 25;
 
-int* currentPosition = new int[3];
+const String CALIBRATE_COMMAND = "calibrate";
+const String FINISH_COMMAND = "finish";
+
+const String CLOSEST_SIDE_MESSAGE = "ClosestSide: ";
+const String NOT_CALIBRATED_MESSAGE = "NotCalibrated";
+
+bool _calibrated;
+
+int _sidesCount;
+int** _sidesData;
+int* _currentPosition;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
-  currentPosition[0] = 0;
-  currentPosition[1] = 0;
-  currentPosition[2] = 0;
+  _calibrated = getCalibrated();
+
+  if (_calibrated) {
+    _sidesCount = getSidesCount();
+    _sidesData = getSides(_sidesCount);
+  }
+  
+  _currentPosition = new int[3];
+  _currentPosition[0] = 0;
+  _currentPosition[1] = 0;
+  _currentPosition[2] = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  bool calibrationMode = shouldCalibrate();
-
-  if (calibrationMode) {
+  if (requestedCalibration()) {
     calibrate();
   } else {
     track();
   }
 
-  delay(1000);
+  delay(INTERVAL);
 }
 
-bool shouldCalibrate() {
+bool requestedCalibration() {
   if (Serial.available() <= 0) {
     return false;
   }
 
   String receivedString = Serial.readString();
-  return receivedString.indexOf("calibrate") != -1;
+  return contains(receivedString, CALIBRATE_COMMAND);
 }
 
 void calibrate() {
-  Serial.println("Entered calibration mode.");
+  log("Entered calibration mode.");
 
   bool finished = false;
   int** sides = new int*[MAX_SIDE_COUNT];
-  int configuredSides = 0; 
+  int sidesCount = 0;
 
   while (finished == false) {
     String input = readInput();
-    
-    if (input.indexOf("finish") != -1) {
+
+    if (contains(input, FINISH_COMMAND)) {
       finished = true;
     } else {
       int* vector = parseVector(input);
-      sides[configuredSides] = vector;
-      configuredSides++;
+      sides[sidesCount] = vector;
+      sidesCount++;
   
-      Serial.println("Configured side:");
-      Serial.print("X=");
-      Serial.print(vector[0]);
-      Serial.print("Y=");
-      Serial.print(vector[1]);
-      Serial.print("Z=");
-      Serial.print(vector[2]);
-      Serial.println();
+      log("Configured side:");
+      log(vector);
     }
   }
 
-  Serial.println("Saving sides:");
-  Serial.println(configuredSides);
-
+  _calibrated = true;
   setCalibrated(true);
-  setSides(sides, configuredSides);
 
+  setSides(sides, sidesCount);
+
+  delete _sidesData;
+  _sidesData = sides;
+  _sidesCount = sidesCount;
   Serial.println("Calibration finished.");
 }
 
 void track() {
-  bool calibrated = getCalibrated();
-  if (calibrated) {
-    int sidesCount = getSidesCount();
-    int** sideData = getSides(sidesCount);
-
-    int lowestDistance = INT_MAX_VALUE;
-    int closestSide = -1;
-
-    for (int i = 0; i < sidesCount; i++) {
-      int newDistance = pow(sideData[i][0] - currentPosition[0], 2) 
-        + pow(sideData[i][1] - currentPosition[1], 2) 
-        + pow(sideData[i][2] - currentPosition[2], 2);
-
-      if (newDistance < lowestDistance) {
-        lowestDistance = newDistance;
-        closestSide = i;
-      }
-    }
-
-    Serial.println("Closest side:");
-    Serial.println(closestSide);
-  } else {
-    Serial.println("The cube has not been calibrated.");
+  if (_calibrated == false) {
+    Serial.println(NOT_CALIBRATED_MESSAGE);
+    return;
   }
+
+  readCurrentPosition();
+  
+  int lowestDistance = INT_MAX_VALUE;
+  int closestSide = -1;
+
+  int newDistance;
+
+  for (int side = 0; side < _sidesCount; side++) {
+    newDistance = calculateDistance(side);
+
+    if (newDistance < lowestDistance) {
+      lowestDistance = newDistance;
+      closestSide = side;
+    }
+  }
+
+  Serial.print(CLOSEST_SIDE_MESSAGE);
+  Serial.println(closestSide);
+}
+
+void readCurrentPosition() {
+  // TODO: Return data from the accelerometer/gyroscope
+}
+
+int calculateDistance(int side) {
+  return pow(_sidesData[side][0] - _currentPosition[0], 2) 
+    + pow(_sidesData[side][1] - _currentPosition[1], 2) 
+    + pow(_sidesData[side][2] - _currentPosition[2], 2);
+}
+
+// Memory handling
+
+const int CALIBRATED_ADDRESS = 0;
+const byte CALIBRATED_FLAG = 178;
+
+const int SIDE_COUNT_ADDRESS = 1;
+
+bool getCalibrated() {
+  return eeprom_readByte(CALIBRATED_ADDRESS) == CALIBRATED_FLAG;
+}
+
+void setCalibrated(bool calibrated) {
+  if (calibrated) {
+    eeprom_writeByte(CALIBRATED_ADDRESS, CALIBRATED_FLAG);
+  } else {
+    eeprom_writeByte(CALIBRATED_ADDRESS, 0);
+  }
+}
+
+byte getSidesCount() {
+  return eeprom_readByte(SIDE_COUNT_ADDRESS);
+}
+
+int** getSides(int sideCount) {
+  int** output = new int*[sideCount];
+
+  for (int i = 0; i < sideCount; i++) {
+    int* vector = new int[3];
+    int sideAddress = getSideAddress(i);
+
+    vector[0] = eeprom_readInt(sideAddress);
+    vector[1] = eeprom_readInt(sideAddress + 2);
+    vector[2] = eeprom_readInt(sideAddress + 4);
+
+    output[i] = vector;
+
+    log("Read side:");
+    log(vector);
+  }
+
+  return output;
+}
+
+void setSides(int** sideData, byte count) {
+  eeprom_writeByte(SIDE_COUNT_ADDRESS, count);
+
+  for (int i = 0; i < count; i++) {
+    int* vector = sideData[i];
+    int sideAddress = getSideAddress(i);
+    
+    eeprom_writeInt(sideAddress, vector[0]);
+    eeprom_writeInt(sideAddress + 2, vector[1]);
+    eeprom_writeInt(sideAddress + 4, vector[2]);
+  }
+}
+
+int getSideAddress(int side) {
+  return side * 6 + SIDE_COUNT_ADDRESS + 1;
+}
+
+// EEPROM
+
+byte eeprom_readByte(int address) {
+  return EEPROM.read(address);
+}
+
+int eeprom_readInt(int address) {
+  byte byte1 = EEPROM.read(address);
+  byte byte2 = EEPROM.read(address + 1);
+
+  return (byte1 << 8) + byte2;
+}
+
+void eeprom_writeByte(int address, byte value) {
+  EEPROM.write(address, value);
+}
+
+void eeprom_writeInt(int address, int value) {
+  EEPROM.write(address, value >> 8);
+  EEPROM.write(address + 1, value & 0xFF);
+}
+
+// Utilities
+
+bool contains(String value, String check) {
+  return value.indexOf(check) != -1;
+}
+
+void log(String message) {
+  Serial.println(message);
+}
+
+void log(int* vector) {
+  for (int i = 0; i < 3; i++) {
+    Serial.print(vector[i]);
+    Serial.print(";");
+  }
+
+  Serial.println();
+}
+
+int* parseVector(String string) {
+  String* elements = splitString(string, ' ');
+  int* output = new int[3];
+
+  for (int i = 0; i < 3; i++) {
+    output[i] = elements[i].toInt();
+  }
+
+  delete elements;
+  return output;
 }
 
 String readInput() {
@@ -109,88 +241,19 @@ String readInput() {
   return Serial.readString();
 }
 
-int* parseVector(String string) {
-  String* elements = splitString(string);
-  int* output = new int[3];
-  output[0] = elements[0].toInt();
-  output[1] = elements[1].toInt();
-  output[2] = elements[2].toInt();
-
-  return output;
-}
-
-String* splitString(String value) {
+String* splitString(String value, char delimiter) {
   String* output = new String[3];
   int valueLength = value.length();
-  int currentElement = 0;
+  int elements = 0;
   int currentIndex = 0;
 
   for (int i = 0; i < valueLength; i++) {
-    if (value[i] == ' ') {
-      output[currentElement] = value.substring(currentIndex, i);
-      currentElement++;
+    if (value[i] == delimiter) {
+      output[elements] = value.substring(currentIndex, i);
+      elements++;
       currentIndex = i;
     }
   }
 
   return output;
-}
-
-// Memory handling
-
-const int CALIBRATED_ADDRESS = 0;
-const int CALIBRATED_FLAG = 178;
-
-const int SIDE_COUNT_ADDRESS = 1;
-
-bool getCalibrated() {
-  int flag = EEPROM.read(CALIBRATED_ADDRESS);
-  return flag == CALIBRATED_FLAG;
-}
-
-void setCalibrated(bool calibrated) {
-  if (calibrated) {
-    EEPROM.write(CALIBRATED_ADDRESS, CALIBRATED_FLAG);
-  } else {
-    EEPROM.write(CALIBRATED_ADDRESS, 0);
-  }
-}
-
-int getSidesCount() {
-  return EEPROM.read(SIDE_COUNT_ADDRESS);
-}
-
-int** getSides(int sideCount) {
-  int** output = new int*[sideCount];
-
-  for (int i = 0; i < sideCount; i++) {
-    int* vector = new int[3];
-    vector[0] = EEPROM.read(i * 6 + SIDE_COUNT_ADDRESS + 1);
-    vector[1] = EEPROM.read(i * 6 + SIDE_COUNT_ADDRESS + 3);
-    vector[2] = EEPROM.read(i * 6 + SIDE_COUNT_ADDRESS + 5);
-
-    output[i] = vector;
-
-    Serial.println("Read side:");
-    Serial.print("X=");
-    Serial.print(vector[0]);
-    Serial.print("Y=");
-    Serial.print(vector[1]);
-    Serial.print("Z=");
-    Serial.print(vector[2]);
-    Serial.println();
-  }
-
-  return output;
-}
-
-void setSides(int** sideData, int count) {
-  EEPROM.write(SIDE_COUNT_ADDRESS, count);
-
-  for (int i = 0; i < count; i++) {
-    int* vector = sideData[i];
-    EEPROM.write(i * 6 + SIDE_COUNT_ADDRESS + 1, vector[0]);
-    EEPROM.write(i * 6 + SIDE_COUNT_ADDRESS + 3, vector[1]);
-    EEPROM.write(i * 6 + SIDE_COUNT_ADDRESS + 5, vector[2]);
-  }
 }
